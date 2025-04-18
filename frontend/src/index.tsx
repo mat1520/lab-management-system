10e065d74efe598ff9cd7d1825afebdb5410c582
mat1520 <azaazay09@gmail.com>
2025-04-18 17:07:16 -0500

    fix: Update react-query to @tanstack/react-query

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import { QueryClient, QueryClientProvider } from 'react-query';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import App from './App';
 import { store } from './store';
 import './index.css';
