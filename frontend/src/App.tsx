310ccb4d7ccd574af68d436e0c9ef394a8cbf3ce
mat1520 <azaazay09@gmail.com>
2025-04-18 17:14:32 -0500

    fix: Add ThemeProvider to App component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,14 +2,17 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Layout from './components/Layout';
 import Home from './pages/Home';
+import { ThemeProvider } from './contexts/ThemeContext';
 
 const App: React.FC = () => {
   return (
-    <Layout>
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
-    </Layout>
+    <ThemeProvider>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Layout>
+    </ThemeProvider>
   );
 };
 
